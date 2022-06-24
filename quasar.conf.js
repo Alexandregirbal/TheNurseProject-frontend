9628c171e8d2cf043e5e14005b0c8eb379a71ec0
Alexandre Girbal <alexandre.girbal.pro@gmail.com>
2022-06-24 19:42:42 +0200

    1st deploy
    install quasar cli on deploy
    
    try to deploy
    
    
    try 2
    
    
    update engine versions
    
    
    use npx to install quasar cli
    
    
    try something else
    
    
    bon c'est relou
    
    
    aller casse les couilles ce front

--- quasar.conf.js
+++ quasar.conf.js
@@ -51,7 +51,7 @@ module.exports = configure(function (ctx) {
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
       env: require('dotenv').config().parsed,
-      vueRouterMode: 'hash', // available values: 'hash', 'history'
+      vueRouterMode: 'history', // available values: 'hash', 'history'
 
       // transpile: false,
       // publicPath: '/',
@@ -114,7 +114,7 @@ module.exports = configure(function (ctx) {
 
     // https://quasar.dev/quasar-cli/developing-ssr/configuring-ssr
     ssr: {
-      pwa: false,
+      pwa: true,
 
       // manualStoreHydration: true,
       // manualPostHydrationTrigger: true,
